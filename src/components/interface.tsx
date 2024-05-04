b14d7354ec7f464b276b74f97a1cf596cb9f08d5
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-05-04 22:03:17 +0530

    Initial Commit

---
+++ src/components/interface.tsx
@@ -0,0 +1,10 @@
+import {FC} from "react";{}
+const Interface: FC = () => {
+    return ( 
+        <div className="interface">
+            
+        </div>
+     );
+}
+ 
+export default Interface;
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+import Interface from './components/interface'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Interface/>
+  </React.StrictMode>,
+)
