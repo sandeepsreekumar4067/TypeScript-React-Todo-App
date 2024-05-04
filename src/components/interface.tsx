ecdd344b87fbeb322d8e7244eb0113499373e256
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-05-04 23:41:56 +0530

    finished the basic UI

--- src/components/interface.tsx
+++ src/components/interface.tsx
@@ -1,15 +1,20 @@
-import { FC, useState } from "react";
+import { ChangeEvent, FC, useState } from "react";
 import backgroudimage from '../assets/backgroundmountain3.jpg';
 import '../style/interface.css';
 import tickMark from '../assets/checkmark-512.png'
-
+import arrow from '../assets/arrow.png'
 
 const Interface: FC = () => {
-    const [isChecked, setIsChecked] = useState(false);
-
+    const [isChecked, setIsChecked] = useState<boolean>(false);
+    const [itemlist,additemlist] = useState<string>('')
+    const [value,setValue] = useState<string>('')
+    const handleValueChange = (event:ChangeEvent<HTMLInputElement>)=>{
+        setValue(event.target.value)
+    }
     const handleCheckboxChange = () => {
         setIsChecked(!isChecked);
     };
+    
     return ( 
         <div className="interface">
             <div className="mountain-background">
@@ -21,9 +26,13 @@ const Interface: FC = () => {
                         <div className={`custom-checkBox ${isChecked?'checked':''}`} onClick={handleCheckboxChange}>
                             <img src={tickMark} alt="" />
                         </div>
+                        <input type="text" placeholder="Enter the Task"/>
+                        <div className="button">
+                            <img src={arrow} />
+                        </div>
                     </div>
                     <div className="task-list">
-                        
+
                     </div>
                 </div>
             </div>
