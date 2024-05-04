dc4d8b40a3a39bc51a0a03e52185edeb66d6de08
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-05-04 23:17:13 +0530

    completed adding the second component div

--- src/components/interface.tsx
+++ src/components/interface.tsx
@@ -1,17 +1,31 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import backgroudimage from '../assets/backgroundmountain3.jpg';
 import '../style/interface.css';
+import tickMark from '../assets/checkmark-512.png'
+
 
 const Interface: FC = () => {
+    const [isChecked, setIsChecked] = useState(false);
+
+    const handleCheckboxChange = () => {
+        setIsChecked(!isChecked);
+    };
     return ( 
         <div className="interface">
             <div className="mountain-background">
                 <img src={backgroudimage} alt="" />
                 <div className="gradient"></div>
-                <div className="overlay"></div>
-            </div>
-            <div className="main-components">
-                
+                <div className="overlay">
+                    <div className="title">TODO</div>
+                    <div className="input-box">
+                        <div className={`custom-checkBox ${isChecked?'checked':''}`} onClick={handleCheckboxChange}>
+                            <img src={tickMark} alt="" />
+                        </div>
+                    </div>
+                    <div className="task-list">
+                        
+                    </div>
+                </div>
             </div>
         </div>
      );
