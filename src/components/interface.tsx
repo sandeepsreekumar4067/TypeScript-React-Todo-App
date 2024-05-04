1a5ad943276c19b8ee1e4e3e1e1f4cc0e276d85d
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-05-04 22:33:37 +0530

    added the overlay component

--- src/components/interface.tsx
+++ src/components/interface.tsx
@@ -1,10 +1,20 @@
-import {FC} from "react";{}
+import { FC } from "react";
+import backgroudimage from '../assets/backgroundmountain3.jpg';
+import '../style/interface.css';
+
 const Interface: FC = () => {
     return ( 
         <div className="interface">
-            
+            <div className="mountain-background">
+                <img src={backgroudimage} alt="" />
+                <div className="gradient"></div>
+                <div className="overlay"></div>
+            </div>
+            <div className="main-components">
+                
+            </div>
         </div>
      );
 }
- 
+
 export default Interface;
