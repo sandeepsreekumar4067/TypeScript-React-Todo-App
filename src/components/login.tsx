de3a93c15e25968a65e9b586177e7b978a58a572
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-05-05 13:41:54 +0530

    added login image container

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,8 +1,24 @@
 import { FC } from "react";
+import '../style/login.css'
+import loginImage from '../assets/login-container.jpg'
 const Loginpage:FC = () => {
     return ( 
         <div>
-            
+            <div className="login-interface">
+                <div className="login-component">
+
+                </div>
+                <div className="image-container">
+                    
+                    <img src={loginImage} alt="" />
+                    <div className="gradient">
+                    <div className="title">LOGIN</div>
+                    <div className="subtitle">
+                        Welcome Back
+                    </div>
+                    </div>
+                </div>
+            </div>
         </div>
      );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 
 import './index.css'
 import Interface from './components/interface'
+import Loginpage from './components/login'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Interface/>
+    <Loginpage/>
   </React.StrictMode>,
 )
